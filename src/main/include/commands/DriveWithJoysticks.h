65ba79df5dd69a340bf74ead5bb2091c87aa86da
lolshoc <lolshoc@gmail.com>
2020-01-23 13:32:00 -0800

    removed unneeded auto-generated comments

--- src/main/include/commands/DriveWithJoysticks.h
+++ src/main/include/commands/DriveWithJoysticks.h
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 #pragma once
 
 #include <frc2/command/CommandBase.h>
@@ -12,20 +5,8 @@
 
 #include "subsystems/DriveTrain.h"
 
-/**
- * An example command that uses an example subsystem.
- *
- * <p>Note that this extends CommandHelper, rather extending CommandBase
- * directly; this is crucially important, or else the decorator functions in
- * Command will *not* work!
- */
 class DriveWithJoysticks : public frc2::CommandHelper<frc2::CommandBase, DriveWithJoysticks> {
  public:
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
   explicit DriveWithJoysticks(DriveTrain* driveTrain);
 
  private:
