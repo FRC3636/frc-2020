6c458a2cf8158a32e9baf43873ace2a77b6dea42
gagnonsilas <gagnon.silas@gmail.com>
2020-02-15 15:02:53 -0800

    now builds

--- src/main/cpp/commands/DriveWithCamera.cpp
+++ src/main/cpp/commands/DriveWithCamera.cpp
@@ -5,20 +5,20 @@ DriveWithCamera::DriveWithCamera(DriveTrain* driveTrain, NetworkTableHandler* ne
   AddRequirements({driveTrain, network});
 }
 
-void TurnWithCamera::Initialize() {
+void DriveWithCamera::Initialize() {
   m_driveTrain->m_drivePID.setSetPoint(BOT_POSITION);
   m_driveTrain->tankDrive(0, 0);
 }
 
-void TurnWithCamera::Execute() {
+void DriveWithCamera::Execute() {
   double v = m_driveTrain->m_drivePID.calculate(m_network->getDistance());
   m_driveTrain->tankDrive(v, v);
 }
 
-void TurnWithCamera::End(bool interrupted) {
+void DriveWithCamera::End(bool interrupted) {
   m_driveTrain->tankDrive(0, 0);
 }
 
-bool TurnWithCamera::IsFinished() {
+bool DriveWithCamera::IsFinished() {
   return std::abs(m_driveTrain->m_drivePID.calculate(m_network->getDistance())) < 0.05;
 }
